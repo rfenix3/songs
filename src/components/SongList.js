7f5106ec35b93014d782af6a2f65b9c029715d49
Richard Fenix <rfenix@Richards-MBP.attlocal.net>
2019-07-03 16:41:13 -0400

    Create and display song list from redux state.

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class SongList extends Component {
+  renderList() {
+    return this.props.songs.map((song) => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button className="ui button primary">
+              Select
+            </button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      )
+    })
+  }
+
   render() {
     // this.props === {songs: state.songs } are made 
     // available in this function due to mapStateToProps
-    console.log(this.props);
-    return <div>SongList</div>
+    // console.log(this.props.songs);
+    return <div className="ui divided list">{this.renderList()}</div>
   }
 }
 
